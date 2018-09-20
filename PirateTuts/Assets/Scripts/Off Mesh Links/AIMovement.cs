822f5589bc261356cc2dc744b4a51de1405caf03
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2018-09-19 21:24:17 -0400

    Tuts First SetUp

---
+++ PirateTuts/Assets/Scripts/Off Mesh Links/AIMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour {
+
+    // Use this for initialization
+
+    private NavMeshAgent agent;
+    public Transform location;
+
+    
+
+
+    
+	void Start () {
+
+        agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray))
+            {
+                Debug.Log("Get position " + Input.mousePosition);
+                agent.SetDestination(Input.mousePosition);
+            }
+        }
+
+        agent.SetDestination(location.position);
+
+	}
+}
